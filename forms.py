1b4aba56fab5fd62554559177d043cdc5b8efc9d
Andrei <Andrej.master@gmail.com>
2022-01-30 01:13:12 +0300

    refresh

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, PasswordField
-from wtforms.validators import DataRequired, Email
-
-class LoginForm(FlaskForm):
-    login = StringField("Login: ", validators=[DataRequired()])
-    password = PasswordField("Password: ", validators=[DataRequired()])
-    submit = SubmitField("Submit")
