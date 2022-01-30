fa7f624d5aa98e52cb10ab96fa2d7bba300e6c09
Andrei <Andrej.master@gmail.com>
2022-01-30 03:20:36 +0300

    update

--- trello-notification.py
+++ trello-notification.py
@@ -1,25 +1,27 @@
 from datetime import datetime, date, time
 from client import client
 
+#
+# settings your API connect in client.py
+#
 # from trello import TrelloClient
 # client = TrelloClient(
 #     api_key='your-API-key',
 #     token='your-token'
 # )
 
-
-today = datetime(2022, 1, 26, 16, 0, 0)
-
-def main():
-    all_boards = client.list_boards()
-
-    for board in all_boards:
-        cards = board.open_cards()
-        for card in cards:
-            date_card = card.created_date.replace(tzinfo=None)
-            if today < date_card:
-                print("%s %s %s" %(card.board.name, card.name, date_card))
+# last datetime update
+last_date = datetime(2022, 1, 26, 16, 0, 0)
 
 
-if __name__ == "__main__":
-    main()
+# all boards in Trello
+all_boards = client.list_boards()
+
+for board in all_boards:
+    # all card in board
+    for card in board.open_cards():
+        date_card = card.created_date.replace(tzinfo=None)
+
+        # select new card
+        if last_date < date_card:
+            print("%s %s %s" %(card.board.name, card.name, date_card))
